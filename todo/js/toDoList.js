ae3f598e3a20e11e4df8890cd8b99d7e79a3112f
ksantos16 <u121364866@byui.edu>
2022-05-23 20:33:38 -0400

    week 5

---
+++ todo/js/toDoList.js
@@ -0,0 +1,5 @@
+import makeToDoTasks from "./utilities.js";
+
+const taskList = document.querySelector(".toDoList");
+const myTaskList = new makeToDoTasks(list, "todo");
+
---
+++ todo/js/utilities.js
@@ -0,0 +1,34 @@
+import { qs, writeToLS, readFromLS, bindTouch } from "./ls.js";
+
+
+
+
+let toDoList = null;
+
+function makeToDoTasks() {
+
+}
+
+function showCompletedTask() {
+
+}
+
+function showActiveTasks() {
+
+}
+
+function countActiveTasks() {
+
+}
+
+function removeTask() {
+
+}
+
+
+function submitTask() {
+    const form = document.forms['hero'];
+    form.addEventListener('submit', makeHero, false);
+}
+
+
