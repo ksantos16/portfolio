9f9d79a49759685d90918bab11fa7c66646a06f1
ksantos16 <u121364866@byui.edu>
2022-07-12 22:32:47 -0400

    Storage file clean up

--- reciepefinder/js/storage.js
+++ reciepefinder/js/storage.js
@@ -1,6 +1,3 @@
-export function qs(selectorName) {
-    return document.querySelector(selectorName);
-}
 
 export function readFromLS(key) {
     return JSON.parse(localStorage.getItem(key));
@@ -8,9 +5,4 @@ export function readFromLS(key) {
 
 export function writeToLS(key, data) {
     localStorage.setItem(key, JSON.stringify(data));
-}
-
-export function bindTouch(selector, callback) {
-    const element = qs(selector);
-    element.addEventListener("click", callback);
 }
