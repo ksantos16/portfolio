a4ede4f1a50234dcd191e01ea6e4dee692ec7842
ksantos16 <u121364866@byui.edu>
2022-07-12 22:33:14 -0400

    1

--- reciepefinder/js/storage.js
+++ reciepefinder/js/storage.js
@@ -1,8 +1,11 @@
 
+export function writeToLS(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
 export function readFromLS(key) {
     return JSON.parse(localStorage.getItem(key));
 }
 
-export function writeToLS(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-}
+
+
