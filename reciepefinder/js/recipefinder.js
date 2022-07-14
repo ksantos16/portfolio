c40b47447833aac6b1dc43926c20db69e0032835
ksantos16 <u121364866@byui.edu>
2022-07-14 15:50:15 -0400

    got rid of the class

---
+++ reciepefinder/js/recipefinder.js
@@ -0,0 +1,13 @@
+
+const savedRecipes = document.querySelector(".favorite-btn");
+
+const mySavedRecipes = new GetSaveRecipes("recipe");
+
+savedRecipes.addEventListener("click", mySavedRecipes);
+
+
+
+
+
+
+
